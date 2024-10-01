d4f53e084e242335bb8e6b9adb740b2bc9c97fca
Jacqueline <jcasalia@yahoo.com.br>
2024-10-01 10:20:53 -0300

    Modo Escuro/Claro:  alternância entre temas escuro e claro na aplicação.

---
+++ frontend/src/components/Dark.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {useDark , useToggle} from '@vueuse/core'
+const isDark=useDark({
+    // onChanged(isDark){
+    //     if(isDark){
+    //         document.documentElement.classList.add('dark')
+    //     }else{
+    //         document.documentElement.classList.remove('dark')
+    //     }
+    // }
+    // selector:'body',
+    // attribute:'color-scheme',
+    // valueDark:'custom-dark',
+    // valueLight:'custom-light'
+});
+const toggleDark=useToggle(isDark);
+
+</script>
+<template>
+    <button 
+    @click="toggleDark()"
+    class="px-4 py-2 bg-green-500 text-white  rounded dark:bg-purple-500"
+    >Dark Mode</button>
+    </template>
